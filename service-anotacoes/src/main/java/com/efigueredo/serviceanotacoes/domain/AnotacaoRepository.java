b061591a158a933863b1410807d78507dd147dfb
efigueredo <emanoel.figueredo.1@gmail.com>
2023-03-30 11:19:03 -0300

    Revert "crud do microserviço de anotações finalizado."
    This reverts commit aa765528403294a172b037a22b3193e8e80d1066.

--- service-anotacoes/src/main/java/com/efigueredo/serviceanotacoes/domain/AnotacaoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.efigueredo.serviceanotacoes.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnotacaoRepository extends JpaRepository<Anotacao, Long> {
-
-}
--- service-anotacoes/src/main/resources/db/migration/V1__create-table-anotacoes.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE anotacoes (
-    id BIGINT(20) NOT NULL AUTO_INCREMENT,
-    titulo VARCHAR(50) NOT NULL,
-    conteudo VARCHAR(2000) NOT NULL,
-    momento DATETIME NOT NULL,
-    PRIMARY KEY(id)
-);
